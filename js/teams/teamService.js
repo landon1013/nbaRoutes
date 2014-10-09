8479c9d9e787ef065c7c08f8d65565add724b21a
Landon1013 <landon@thecallfamily.com>
2014-10-09 17:02:05 -0600

    adds teamService.js

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,47 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+    var addNewGame = function(gameObj){
+        var url = "http://api.parse.com/1/classes/" + gameObj.homeTeam;
+
+        if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+            gameObj.won = true;
+        }
+        else {
+            gameObj.won = false;
+        }
+
+        return $http({method: "GET", url: url, data: gameObj});
+    };
+
+    var getTeamData = function(team){
+
+        var deferred = $q.defer();
+
+        var url = "https://api.parse.com/1/classes/" + team;
+
+        $http({method: "GET", url: url}).then(function(data){
+            var results = data.data.results;
+            var wins = 0;
+            var losses = 0;
+
+            for(var i = 0; i < results.length; i++) {
+                if (results.won[i] === true) {
+                    wins = wins + 1;
+                }
+                else {
+                    losses = losses + 1;
+                }
+
+                results.push(wins, losses);
+
+                deferred.resolve(results);  
+            }
+        });
+
+        return deferred.promise;
+        };
+
+//ellen moffatt
+
 });
